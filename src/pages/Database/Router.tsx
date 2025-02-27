c972994d232adf1c2c7d2fd1aa212c7dcc4ae09b
PabloRius <pablogrius@gmail.com>
2025-02-27 19:40:28 +0000

    Fix database router to match the new routing system, delete unsued files

--- src/pages/Database/Router.tsx
+++ src/pages/Database/Router.tsx
@@ -1,35 +1,31 @@
-import { Route, Routes } from "react-router";
 import { Game } from "@database.types";
-import { ImageNav } from "@components/navs/ImageNav";
 
-import { MainRouter } from "./IEGOCS/Router";
+import { useGame } from "@/hooks/useGame";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { MainRouter as MainIEGOCSRouter } from "./IEGOCS/Router";
 
 export function DatabaseRouter() {
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <ImageNav
-            tables={{
-              IE: Game.IE,
-              IE2: Game.IE2,
-              IE3: Game.IE3,
-              IEGO: Game.IEGO,
-              IEGOCS: Game.IEGOCS,
-              IEGOGA: Game.IEGOGA,
-              IEHVR: Game.IEHVR,
-            }}
-          />
-        }
-      />
-      {/* <Route path="/map/IE" element={<Main />} /> */}
-      {/* <Route path="/map/IE2" element={<Main />} /> */}
-      {/* <Route path="/map/IE2" element={<Main />} /> */}
-      {/* <Route path="/map/IEGO" element={<Main />} /> */}
-      <Route path="IEGOCS/*" element={<MainRouter />} />
-      {/* <Route path="/map/IEGOGA" element={<Main />} /> */}
-      {/* <Route path="/map/IEVR" element={<Main />} /> */}
-    </Routes>
-  );
+  const navigate = useNavigate();
+  const { selectedGame } = useGame();
+
+  useEffect(() => {
+    if (!selectedGame) {
+      navigate("/select-game");
+    }
+  }, [selectedGame, navigate]);
+
+  if (!selectedGame) {
+    return null;
+  }
+
+  switch (selectedGame) {
+    case Game.IEGOCS:
+      return <MainIEGOCSRouter />;
+
+    default:
+      return (
+        <div className="text-center">{selectedGame} Not implemented yet</div>
+      );
+  }
 }
