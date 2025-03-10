4b766d5da18e49c6c0ed6fd85d04da2c8951a382
PabloRius <pablogrius@gmail.com>
2025-03-10 19:28:52 +0000

    Add new players, characters and teams

--- src/database/characters.ts
+++ src/database/characters.ts
@@ -2,9 +2,34 @@ import { Character } from "@database.types";
 import { teams } from ".";
 
 export const characters: Record<number, Character> = {
-  0: new Character(0, "Mark Evans", "Mamoru Endou", "male", [
-    teams[0],
+  0: new Character(1, "Arion Sherwind", "Tenma Matsukaze", "male", [
     teams[1],
+    teams[2],
+    teams[3],
+    teams[4],
+    teams[5],
   ]),
-  1: new Character(1, "Arion Sherwind", "Tenma Matsukaze", "male", [teams[1]]),
+  1: new Character(2, "Victor Blade", "Kyosuke Tsurugi", "male", [
+    teams[1],
+    teams[3],
+    teams[4],
+    teams[5],
+    teams[6],
+    teams[7],
+  ]),
+  2: new Character(2, "Samguk Han", "Sangoku Taichi", "male", [teams[1]]),
+  3: new Character(3, "Subaru Honda", "Kurumada Gouichi", "male", [teams[1]]),
+  4: new Character(4, "Gabriel Garcia", "Kirino Ranmaru", "male", [
+    teams[1],
+    teams[3],
+    teams[4],
+  ]),
+  5: new Character(5, "Wanli Changcheng", "Amagi Daichi", "male", [teams[1]]),
+  6: new Character(6, "Jean-Pierre Lapin", "Nishinozo Shinsuke", "male", [
+    teams[1],
+    teams[1],
+    teams[3],
+    teams[4],
+    teams[5],
+  ]),
 };
--- src/database/teams.ts
+++ src/database/teams.ts
@@ -4,4 +4,14 @@ import { Team } from "../types/database";
 export const teams: Record<number, Team> = {
   0: new Team(0, "Raimon", getImageUrl("teams/Raimon.png")),
   1: new Team(1, "Raimon (GO)", getImageUrl("teams/RaimonGO.png")),
+  2: new Team(2, "Tenmas", getImageUrl("teams/Tenmas.png")),
+  3: new Team(3, "New Inazuma Japan", getImageUrl("teams/NewInazumaJapan.png")),
+  4: new Team(4, "Chrono Storm", getImageUrl("teams/ChronoStorm.png")),
+  5: new Team(5, "Earth Eleven", getImageUrl("teams/EarthEleven.png")),
+  6: new Team(
+    6,
+    "Order of Black Templars",
+    getImageUrl("teams/OrderOfBlackTemplars.png")
+  ),
+  7: new Team(7, "Falam Medius", getImageUrl("teams/FalamMedius.png")),
 };
