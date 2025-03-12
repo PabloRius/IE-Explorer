c930ac456bb07688d92a2c54503eb3d507544b10
PabloRius <pablogrius@gmail.com>
2025-03-12 15:26:48 +0000

    Add smaller version of the arrow

--- src/components/icons/Arrow.tsx
+++ src/components/icons/Arrow.tsx
@@ -4,13 +4,21 @@ export function Arrow({
   x,
   y,
   direction,
+  size,
 }: {
   x: number;
   y: number;
   direction: "up" | "down" | "left" | "right";
+  size?: "sm" | "lg";
 }) {
   return direction === "up" ? (
-    <svg width={15} height={16} viewBox="0 0 30 32" x={x - 7.5} y={y}>
+    <svg
+      width={size ? (size === "sm" ? 7 : 15) : 15}
+      height={size ? (size === "sm" ? 8 : 16) : 16}
+      viewBox="0 0 30 32"
+      x={x - (size ? (size === "sm" ? 3.5 : 7.5) : 7.5)}
+      y={y}
+    >
       <image
         href={getImageUrl("icons/ArrowUp.png")}
         width={30}
@@ -19,7 +27,13 @@ export function Arrow({
       />
     </svg>
   ) : direction === "down" ? (
-    <svg width={15} height={16} viewBox="0 0 30 32" x={x - 7.5} y={y - 16}>
+    <svg
+      width={size ? (size === "sm" ? 7 : 15) : 15}
+      height={size ? (size === "sm" ? 8 : 16) : 16}
+      viewBox="0 0 30 32"
+      x={x - (size ? (size === "sm" ? 3.5 : 7.5) : 7.5)}
+      y={y - (size ? (size === "sm" ? 8 : 16) : 16)}
+    >
       <image
         href={getImageUrl("icons/ArrowDown.png")}
         width={30}
@@ -28,7 +42,13 @@ export function Arrow({
       />
     </svg>
   ) : direction === "left" ? (
-    <svg width={16} height={15} viewBox="0 0 32 30" x={x} y={y - 7.5}>
+    <svg
+      width={size ? (size === "sm" ? 8 : 16) : 16}
+      height={size ? (size === "sm" ? 7 : 15) : 15}
+      viewBox="0 0 32 30"
+      x={x}
+      y={y - (size ? (size === "sm" ? 3.75 : 7.5) : 7.5)}
+    >
       <image
         href={getImageUrl("icons/ArrowLeft.png")}
         width={32}
@@ -37,7 +57,13 @@ export function Arrow({
       />
     </svg>
   ) : (
-    <svg width={16} height={15} viewBox="0 0 32 30" x={x - 16} y={y - 7.5}>
+    <svg
+      width={size ? (size === "sm" ? 8 : 16) : 16}
+      height={size ? (size === "sm" ? 7 : 15) : 15}
+      viewBox="0 0 32 30"
+      x={x - (size ? (size === "sm" ? 8 : 16) : 16)}
+      y={y - (size ? (size === "sm" ? 3.75 : 7.5) : 7.5)}
+    >
       <image
         href={getImageUrl("icons/ArrowRight.png")}
         width={32}
