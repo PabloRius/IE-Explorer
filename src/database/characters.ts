7df44053df0708e40f61d772d103b56ad340744f
PabloRius <pablogrius@gmail.com>
2025-03-14 10:43:55 +0000

    Update data construction method

--- src/database/characters.ts
+++ src/database/characters.ts
@@ -1,35 +1,59 @@
 import { Character } from "@database.types";
 import { teams } from ".";
 
-export const characters: Record<number, Character> = {
-  0: new Character(1, "Arion Sherwind", "Tenma Matsukaze", "male", [
+const characters: Record<number, Character> = {};
+function addCharacter(character: Character) {
+  const { id } = character;
+  if (id in characters) throw new Error("Duplicated character ID");
+  characters[id] = character;
+}
+
+addCharacter(
+  new Character(0, "Arion Sherwind", "Tenma Matsukaze", "male", [
     teams[1],
     teams[2],
     teams[3],
     teams[4],
     teams[5],
-  ]),
-  1: new Character(2, "Victor Blade", "Kyosuke Tsurugi", "male", [
+  ])
+);
+addCharacter(
+  new Character(1, "Victor Blade", "Kyosuke Tsurugi", "male", [
     teams[1],
     teams[3],
     teams[4],
     teams[5],
     teams[6],
     teams[7],
-  ]),
-  2: new Character(2, "Samguk Han", "Sangoku Taichi", "male", [teams[1]]),
-  3: new Character(3, "Subaru Honda", "Kurumada Gouichi", "male", [teams[1]]),
-  4: new Character(4, "Gabriel Garcia", "Kirino Ranmaru", "male", [
+  ])
+);
+
+addCharacter(
+  new Character(2, "Samguk Han", "Sangoku Taichi", "male", [teams[1]])
+);
+
+addCharacter(
+  new Character(3, "Subaru Honda", "Kurumada Gouichi", "male", [teams[1]])
+);
+
+addCharacter(
+  new Character(4, "Gabriel Garcia", "Kirino Ranmaru", "male", [
     teams[1],
     teams[3],
     teams[4],
-  ]),
-  5: new Character(5, "Wanli Changcheng", "Amagi Daichi", "male", [teams[1]]),
-  6: new Character(6, "Jean-Pierre Lapin", "Nishinozo Shinsuke", "male", [
+  ])
+);
+
+addCharacter(
+  new Character(5, "Wanli Changcheng", "Amagi Daichi", "male", [teams[1]])
+);
+
+addCharacter(
+  new Character(6, "Jean-Pierre Lapin", "Nishinozo Shinsuke", "male", [
     teams[1],
     teams[1],
     teams[3],
     teams[4],
     teams[5],
-  ]),
-};
+  ])
+);
