d1b4ac66fb0cd6afdf2e7a6f5ac43277089604d8
PabloRius <pablogrius@gmail.com>
2025-03-16 21:23:43 +0000

    Fix icon missalignment

--- src/components/icons/Photo.tsx
+++ src/components/icons/Photo.tsx
@@ -26,8 +26,8 @@ export function Photo({
       width={15.5}
       height={14}
       viewBox="0 0 31 28"
-      x={x - 3}
-      y={y - 14}
+      x={x - 7.75}
+      y={y - 7}
       className="interactable"
     >
       <image href={avatar} width={31} height={28} />
