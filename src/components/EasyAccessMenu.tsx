1cfae6c3a9922a0033e26b405ebd3697e74b8041
PabloRius <pablogrius@gmail.com>
2025-03-17 13:47:10 +0000

    Add split help menus for map and database

--- src/components/EasyAccessMenu.tsx
+++ src/components/EasyAccessMenu.tsx
@@ -4,32 +4,45 @@ import { Link, useLocation } from "react-router";
 export function EasyAccessMenu() {
   const location = useLocation();
 
+  const [currentLocation, nextLocation] = location.pathname
+    .split("/")
+    .slice(1, 3);
+
   return (
     <menu className="fixed flex flex-row gap-2 justify-center items-center flex-wrap bottom-2 right-2 bg-gray-800 z-50 p-2 rounded shadow-lg">
-      {window.history.length > 1 && (
-        <button
-          onClick={() => window.history.back()}
+      {/* {window.history.length > 1 && (
+        <Link
+          to={"javascript:history.back()"}
+          rel="prev"
           className="flex items-center justify-center"
         >
           <img
             src={getImageUrl("icons/Back.png")}
             className="w-12 aspect-auto transition-transform duration-200 hover:scale-110 hover:opacity-80"
           />
-        </button>
-      )}
-
-      {location.pathname !== "/" && (
-        <Link
-          to=".."
-          relative="path"
-          className="flex items-center justify-center"
-        >
-          <img
-            src={getImageUrl("icons/Map.png")}
-            className="w-12 aspect-auto transition-transform duration-200 hover:scale-110 hover:opacity-80"
-          />
         </Link>
-      )}
+      )} */}
+
+      {(currentLocation === "map" || currentLocation === "database") &&
+        nextLocation && (
+          <Link
+            to=".."
+            relative="path"
+            className="flex items-center justify-center"
+          >
+            {location.pathname.split("/")[1] === "map" ? (
+              <img
+                src={getImageUrl("icons/Map.png")}
+                className="w-12 aspect-auto transition-transform duration-200 hover:scale-110 hover:opacity-80"
+              />
+            ) : (
+              <img
+                src={getImageUrl("icons/Menu.png")}
+                className="w-12 aspect-auto transition-transform duration-200 hover:scale-110 hover:opacity-80"
+              />
+            )}
+          </Link>
+        )}
     </menu>
   );
 }
